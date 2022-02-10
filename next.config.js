e460493bcf0f7c0e55c0798523d67da27e8b41b4
guilhermegonsalveszup <guilherme.gonsalves@zup.com.br>
2022-02-10 19:06:04 -0300

    First Commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   reactStrictMode: true,
+  webpack5: true,
+  webpack: (config, options) => {
+    const { ModuleFederationPlugin } = options.webpack.container;
+    
+    config.plugins.push(
+      new ModuleFederationPlugin({
+        remotes: {
+          nav: "nav@http://localhost:3001/remoteEntry.js",
+        },
+        shared: [],
+      })
+    );
+
+    return config;
+  }
 }
